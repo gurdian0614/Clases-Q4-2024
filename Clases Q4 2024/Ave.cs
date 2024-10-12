ffb85c4d917e49126cbf161449766c53006f9277
gurdian0614 <ivan.gurdian@outlook.com>
2024-10-11 18:49:14 -0600

    Crear clase Ave

---
+++ Clases Q4 2024/Ave.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Clases_Q4_2024
+{
+    public class Ave
+    {
+        public string Nombre { get; set; }
+        public string Especie { get; set; }
+        public string Color { get; set; }
+        public double Tamano { get; set; }
+        public string Habitat { get; set; }
+    }
+}
